6ab0219e37716788194c3a61d7ef5651a39bde54
Desond Anane <doanane@st.ug.edu.gh>
2024-05-18 23:10:15 +0000

    Task 1: Creating the processArray function

---
+++ arrayManipulation.js
@@ -0,0 +1,40 @@
+function transformArray(arr) {
+    let resultArray = [];
+    for (let element of arr) {
+        if (element % 2 === 0) {
+            resultArray.push(element ** 2);
+        } else {
+            resultArray.push(element * 3);
+        }
+    }
+    return resultArray;
+}
+
+// function styleArrayStrings(texts, nums) {
+//     if (texts.length !== nums.length) {
+//         throw new Error("Both arrays must have the same length.");
+//     }
+
+//     let styledStrings = [];
+//     for (let i = 0; i < texts.length; i++) {
+//         if (nums[i] % 2 === 0) {
+//             styledStrings.push(texts[i].toUpperCase());
+//         } else {
+//             styledStrings.push(texts[i].toLowerCase());
+//         }
+//     }
+//     return styledStrings;
+// }
+
+// let wordsArray = ["Hello", "CS students", "JavaScript", "is", "Awesome"];
+// let numsArray = [1, 8, 2, 13, 5];
+// let transformedNumbers = transformArray(numsArray);
+// console.log(transformedNumbers);
+
+// let styledStrings = styleArrayStrings(wordsArray, transformedNumbers);
+// console.log(styledStrings);
+
+// module.exports = {
+//     styleArrayStrings,
+//     transformArray
+// };
